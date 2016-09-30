c6f028f1de087135ab30db136cc02b0d4884706e
Alonso Angel Sanchez Maldonado <alos1895@gmail.com>
2016-09-30 11:24:09 -0500

    Llamadas
    Alonso

--- app/src/main/java/com/example/gerardo/listacontactos/MainActivity.java
+++ app/src/main/java/com/example/gerardo/listacontactos/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.gerardo.listacontactos;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
                 imprimirContactos();
         } else
             imprimirContactos();
+
+
+
     }
 
     private void imprimirContactos() {
@@ -47,6 +57,27 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, contactos);
 
         listaContactos.setAdapter(adapter);
+
+        listaContactos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
+        {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                           int position, long arg3)
+            {
+                String num = listaContactos.getItemAtPosition(position).toString();
+                StringTokenizer llamar = new StringTokenizer(num,"+");
+                llamar.nextToken();
+
+
+                String numero = llamar.nextToken();
+                Toast.makeText(getApplicationContext(), numero, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(numero));
+                startActivity(intent);
+
+                return true;
+            }
+
+        });
     }
 
     private List<String> obtenerContactos() {
